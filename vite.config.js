156d737d36a077bdc7c4ace3a742a60fba952324
Vegeta-7 <soodakshat8@gmail.com>
2025-05-23 16:40:46 +0530

    fix:vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-}
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  build: {
+    outDir: '../dist', // 🔥 Outputs dist folder at root level
+    emptyOutDir: true,
+  },
+})
